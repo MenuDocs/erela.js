cf1ca0f3d7e8ecd7b334783f4ff05fe2109363e8
melike2d <real.melike2d@gmail.com>
2022-09-12 19:16:01 -0700

    chore: better compilation & scripts

--- src/structures/Manager.ts
+++ src/structures/Manager.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-async-promise-executor */
-import Collection from "@discordjs/collection";
+import { Collection } from "@discordjs/collection";
 import { EventEmitter } from "events";
 import { VoiceState } from "..";
 import { Node, NodeOptions } from "./Node";
